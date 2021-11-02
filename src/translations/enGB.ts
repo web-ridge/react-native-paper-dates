a47ca32f83800c73c5b7b20155a448846a00c7f2
Maciek Zmijewski <maciekzmijewski@resethealth.clinic>
2021-11-02 14:28:33 +0000

    feat: Polish and EnglishGB localisation

---
+++ src/translations/enGB.ts
@@ -0,0 +1,15 @@
+import type { TranslationsType } from './utils'
+
+const enGB: TranslationsType = {
+  save: 'Save',
+  selectSingle: 'Select date',
+  selectMultiple: 'Select dates',
+  selectRange: 'Select period',
+  notAccordingToDateFormat: (inputFormat: string) =>
+    `Date format must be ${inputFormat}`,
+  mustBeHigherThan: 'Must be later then',
+  mustBeLowerThan: 'Must be earlier then',
+  mustBeBetween: 'Must be between',
+  dateIsDisabled: 'Day is not allowed',
+}
+export default enGB
