a8622892924d214cb9017127768230111c3fb0e1
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2022-12-25 19:51:32 -0500

    Fix broken test.

--- src/__tests__/Time/AmPmSwitcher.test.tsx
+++ src/__tests__/Time/AmPmSwitcher.test.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
 
 import renderer from 'react-test-renderer'
+import { inputTypes } from '../../Time/timeUtils'
 import AmPmSwitcher from '../../Time/AmPmSwitcher'
 
 it('renders AmPmSwitcher', () => {
   const tree = renderer
-    .create(<AmPmSwitcher hours={12} onChange={() => null} />)
+    .create(
+      <AmPmSwitcher
+        hours={12}
+        onChange={() => null}
+        inputType={inputTypes.keyboard}
+      />
+    )
     .toJSON()
 
   expect(tree).toMatchSnapshot()
