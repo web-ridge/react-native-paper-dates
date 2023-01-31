2f7f548344feb8ebee76368c053ec1c2c743b2db
ilank <ilank@moonsite.co.il>
2023-01-31 23:17:11 +0200

    Adding hebrew translation

---
+++ src/translations/he.ts
@@ -0,0 +1,21 @@
+import type { TranslationsType } from './utils'
+
+const he: TranslationsType = {
+  save: 'שמור',
+  selectSingle: 'בחר תאריך',
+  selectMultiple: 'בחר תאריכים',
+  selectRange: 'בחר טווח',
+  notAccordingToDateFormat: (inputFormat) =>
+    `פורמט של תאריך צריך להיות ${inputFormat}`,
+  mustBeHigherThan: (date) => `חייב להיות אחרי ${date}`,
+  mustBeLowerThan: (date) => `חייב להיות לפני ${date}`,
+  mustBeBetween: (startDate, endDate) =>
+    `חייב להיות בין ${startDate} - ${endDate}`,
+  dateIsDisabled: 'יום לא מורשה',
+  previous: 'הקודם',
+  next: 'הבא',
+  typeInDate: 'הקש תאריך',
+  pickDateFromCalendar: 'בחר תאריך מהלוח שנה',
+  close: 'סגור',
+}
+export default he
