59de956f7177c1c9d960f8747d7cd6cb1bbcf72d
Sharf Mohiuddin <111132563+SharfMohiuddin@users.noreply.github.com>
2023-08-24 14:05:01 +0200

    formate error fixed

--- src/Date/DatePickerModal.tsx
+++ src/Date/DatePickerModal.tsx
@@ -6,13 +6,13 @@ import {
   useWindowDimensions,
   View,
   Platform,
-  StatusBar
+  StatusBar,
 } from 'react-native'
 import { useTheme } from 'react-native-paper'
 import DatePickerModalContent, {
   DatePickerModalContentMultiProps,
   DatePickerModalContentRangeProps,
-  DatePickerModalContentSingleProps
+  DatePickerModalContentSingleProps,
 } from './DatePickerModalContent'
 import { useHeaderBackgroundColor, useHeaderColorIsLight } from '../utils'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -23,7 +23,11 @@ interface DatePickerModalProps {
   disableStatusBar?: boolean
   disableStatusBarPadding?: boolean
   inputEnabled?: boolean
-  presentationStyle?: 'fullScreen' | 'pageSheet' | 'formSheet' | 'overFullScreen'
+  presentationStyle?:
+    | 'fullScreen'
+    | 'pageSheet'
+    | 'formSheet'
+    | 'overFullScreen'
 }
 
 export interface DatePickerModalSingleProps
@@ -59,7 +63,7 @@ export function DatePickerModal(
     animationType ||
     Platform.select({
       web: 'none',
-      default: 'slide'
+      default: 'slide',
     })
 
   const isLight = useHeaderColorIsLight()
@@ -84,16 +88,19 @@ export function DatePickerModal(
               style={[
                 StyleSheet.absoluteFill,
                 styles.modalBackground,
-                { backgroundColor: theme.colors.backdrop }
+                { backgroundColor: theme.colors.backdrop },
               ]}
             />
           </TouchableWithoutFeedback>
-          <View style={[StyleSheet.absoluteFill, styles.modalRoot]} pointerEvents="box-none">
+          <View
+            style={[StyleSheet.absoluteFill, styles.modalRoot]}
+            pointerEvents="box-none"
+          >
             <View
               style={[
                 styles.modalContent,
                 { backgroundColor: theme.colors.surface },
-                dimensions.width > 650 ? styles.modalContentBig : null
+                dimensions.width > 650 ? styles.modalContentBig : null,
               ]}
             >
               {disableStatusBar ? null : (
@@ -107,8 +114,8 @@ export function DatePickerModal(
                   style={[
                     {
                       height: insets.top,
-                      backgroundColor: headerBackgroundColor
-                    }
+                      backgroundColor: headerBackgroundColor,
+                    },
                   ]}
                 />
               )}
@@ -130,29 +137,29 @@ const supportedOrientations: any = [
   'portrait-upside-down',
   'landscape',
   'landscape-left',
-  'landscape-right'
+  'landscape-right',
 ]
 
 const styles = StyleSheet.create({
   modalRoot: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
   },
   modalBackground: {
-    flex: 1
+    flex: 1,
   },
   modalContent: {
     flex: 1,
-    width: '100%'
+    width: '100%',
   },
   modalContentBig: {
     maxWidth: 600,
     maxHeight: 800,
     borderRadius: 10,
     width: '100%',
-    overflow: 'hidden'
-  }
+    overflow: 'hidden',
+  },
 })
 
 export default React.memo(DatePickerModal)
