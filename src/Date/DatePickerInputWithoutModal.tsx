2ee8016f3690b837bbe2ff221d591aede0f63755
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2023-09-03 18:45:10 -0400

    chore: fix linting error

--- src/Date/DatePickerInputWithoutModal.tsx
+++ src/Date/DatePickerInputWithoutModal.tsx
@@ -115,7 +115,7 @@ function DatePickerInputWithoutModal(
         startYear,
         endYear,
         inputEnabled,
-        disableStatusBarPadding
+        disableStatusBarPadding,
       })}
     </>
   )
