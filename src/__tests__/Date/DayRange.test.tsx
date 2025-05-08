ba4cf62b868ff75ecfb2bd2cc1d498d1a402c77f
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2025-05-07 20:14:30 -0400

    Fix linting error

--- src/__tests__/Date/DayRange.test.tsx
+++ src/__tests__/Date/DayRange.test.tsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react-native'
 import DayRange from '../../Date/DayRange'
 
 it('renders DayRange', () => {
-  const { toJSON } = render(<DayRange inRange leftCrop rightCrop selectColor="blue" />)
+  const { toJSON } = render(
+    <DayRange inRange leftCrop rightCrop selectColor="blue" />
+  )
   expect(toJSON()).toMatchSnapshot()
 })
