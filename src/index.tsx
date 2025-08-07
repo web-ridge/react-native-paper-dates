0717a91883dad1904cc871258c4e7a36e6d99910
Robin Lindhout <79744544+robinlindhout@users.noreply.github.com>
2025-08-07 10:29:47 +0200

    Try to fix import in newer expo https://github.com/web-ridge/react-native-paper-dates/issues/490

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 export { default as Calendar } from './Date/Calendar'
 export { default as DatePickerModal } from './Date/DatePickerModal'
-export * from './Date/DatePickerModal'
+export type { 
+  DatePickerModalSingleProps,
+  DatePickerModalMultiProps,
+  DatePickerModalRangeProps
+} from './Date/DatePickerModal'
 export { default as DatePickerModalContent } from './Date/DatePickerModalContent'
 export { default as TimePickerModal } from './Time/TimePickerModal'
 export { default as TimePicker } from './Time/TimePicker'
